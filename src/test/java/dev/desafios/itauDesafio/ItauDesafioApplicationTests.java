ca572af5fd9a1a0b7093b9125ac8aaa60a2fbf77
Luiz Duarte <luizjmduarte@gmail.com>
2026-08-24 20:29:46 -0300

    first commit

---
+++ src/test/java/dev/desafios/itauDesafio/ItauDesafioApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.desafios.itauDesafio;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ItauDesafioApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
